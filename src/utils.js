f02ff2b5ca3388913218722aae0b941c11554dde
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-04-11 16:34:03 -0400

    add file for JS utility functions

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+// TODO: typing effect function
+// https://www.npmjs.com/package/react-typing-effect
+
+// Creates effect of typing out message on screen
+const typeOut = (message) => {};
+
+export default typeOut;
