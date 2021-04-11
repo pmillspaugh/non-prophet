9cd0c61502a49426191d593081aad37bb22ef709
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-04-11 16:39:16 -0400

    render global styles in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import GlobalStyles from './components/GlobalStyles';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
 );
