1db270de4623387cce015529362ab82d65a1c7f0
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-08-12 09:58:39 -0400

    Switch package mgmt from yarn to npm

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-// TODO: typing effect function
-// https://www.npmjs.com/package/react-typing-effect
-
-// Creates effect of typing out message on screen
-const typeOut = (message) => {};
-
-export default typeOut;
