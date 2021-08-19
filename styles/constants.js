c1b938acae87e1c46d5b4072b2d305babd9e4dd7
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-08-19 14:18:41 -0400

    Move global styles to distinct folder

---
+++ styles/constants.js
@@ -0,0 +1,39 @@
+/*
+Colors and color names from palette created/saved on Coolors:
+https://coolors.co/131616-dc9600-e6ebf0-d9dde2-000829
+*/
+
+const COLORS = {
+  eerieBlack: 'hsl(180deg, 7%, 8%)',
+  harvestGold: 'hsl(41deg, 100%, 43%)',
+  aliceBlue: 'hsl(210deg, 25%, 92%)',
+  gainsboro: 'hsl(213deg, 13%, 87%)',
+  oxfordBlue: 'hsl(228deg, 100%, 8%)',
+};
+
+const FONTS = {
+  josefinSans: `'Josefin Sans', sans-serif`,
+};
+
+const WEIGHTS = {
+  100: '100',
+  200: '200',
+  300: '300',
+  400: '400',
+  500: '500',
+  600: '600',
+  700: '700',
+};
+
+const TYPESCALE = {
+  h1: '1.8rem',
+  h2: '1.6rem',
+  h3: '1.4rem',
+  h4: '1.2rem',
+  h5: '1.1rem',
+  p: '1rem',
+  help: '0.8rem',
+  copyright: '0.7rem',
+};
+
+export { COLORS, FONTS, WEIGHTS, TYPESCALE };
