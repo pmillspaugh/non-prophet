ca906783d7ee77ecae6d2a7086919d4acd29ffc2
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-08-21 12:25:05 -0400

    Add meta description and page titles

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Head from 'next/head';
 import MaxWidthWrapper from '../components/layout/MaxWidthWrapper';
 import Landing from '../components/utils/Landing';
 import Spacer from '../components/utils/Spacer';
@@ -6,43 +7,52 @@ import { COLORS, WEIGHTS } from '../styles/constants';
 
 const About = () => {
   return (
-    <MaxWidthWrapper>
-      <Main>
-        <Landing message='About.' />
-        <AboutSection>
-          <h1>
-            <NonProphet>non-prophet</NonProphet> is a <em>not-for-profit</em>{' '}
-            venture.
-          </h1>
-          <Spacer size='36px' />
-          <BodyText>
-            All profits are donated to the{' '}
-            <ExternalLink href='https://eji.org/' target='_blank'>
-              Equal Justice Initiative
-            </ExternalLink>
-            . The EJI is a nonprofit organization challenging racial and
-            economic injustice in the US, with a focus on ending mass
-            incarceration and excessive punishment.
-          </BodyText>
-          <Spacer size='16px' />
-          <BodyText>
-            The site is a work in progress right now, so you won&apos;t be able
-            to add items to your cart, sign up, or checkout just yet.
-          </BodyText>
-          <Spacer size='16px' />
-          <BodyText>
-            If you have any feedback, let me know on{' '}
-            <ExternalLink
-              href='https://twitter.com/pete_millspaugh'
-              target='_blank'
-            >
-              Twitter
-            </ExternalLink>
-            . Thank you for your support!
-          </BodyText>
-        </AboutSection>
-      </Main>
-    </MaxWidthWrapper>
+    <>
+      <Head>
+        <title>non-prophet | About.</title>
+        <meta
+          name='description'
+          content='Welcome to non-prophet | Shop e-commerce, all profit donated to the Equal Justice Initiative'
+        />
+      </Head>
+      <MaxWidthWrapper>
+        <Main>
+          <Landing message='About.' />
+          <AboutSection>
+            <h1>
+              <NonProphet>non-prophet</NonProphet> is a <em>not-for-profit</em>{' '}
+              venture.
+            </h1>
+            <Spacer size='36px' />
+            <BodyText>
+              All profits are donated to the{' '}
+              <ExternalLink href='https://eji.org/' target='_blank'>
+                Equal Justice Initiative
+              </ExternalLink>
+              . The EJI is a nonprofit organization challenging racial and
+              economic injustice in the US, with a focus on ending mass
+              incarceration and excessive punishment.
+            </BodyText>
+            <Spacer size='16px' />
+            <BodyText>
+              The site is a work in progress right now, so you won&apos;t be
+              able to add items to your cart, sign up, or checkout just yet.
+            </BodyText>
+            <Spacer size='16px' />
+            <BodyText>
+              If you have any feedback, let me know on{' '}
+              <ExternalLink
+                href='https://twitter.com/pete_millspaugh'
+                target='_blank'
+              >
+                Twitter
+              </ExternalLink>
+              . Thank you for your support!
+            </BodyText>
+          </AboutSection>
+        </Main>
+      </MaxWidthWrapper>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Head from 'next/head';
 import MaxWidthWrapper from '../components/layout/MaxWidthWrapper';
 import Landing from '../components/utils/Landing';
 import Section from '../components/homepage/Section';
@@ -8,20 +9,29 @@ import SignUp from '../components/homepage/SignUp';
 
 const Home = () => {
   return (
-    <MaxWidthWrapper>
-      <Main>
-        <Landing message='Welcome to non-prophet.' />
-        <Section>
-          <h1>All profits are donated to the Equal Justice Initiative.</h1>
-          <EJIAnchor />
-        </Section>
-        <Section>
-          <h1>Shop.</h1>
-          <ShopContainer />
-        </Section>
-        <SignUp />
-      </Main>
-    </MaxWidthWrapper>
+    <>
+      <Head>
+        <title>non-prophet | Home.</title>
+        <meta
+          name="description"
+          content="Welcome to non-prophet | Shop e-commerce, all profit donated to the Equal Justice Initiative"
+        />
+      </Head>
+      <MaxWidthWrapper>
+        <Main>
+          <Landing message='Welcome to non-prophet.' />
+          <Section>
+            <h1>All profits are donated to the Equal Justice Initiative.</h1>
+            <EJIAnchor />
+          </Section>
+          <Section>
+            <h1>Shop.</h1>
+            <ShopContainer />
+          </Section>
+          <SignUp />
+        </Main>
+      </MaxWidthWrapper>
+    </>
   );
 };
 
